13fcb95cbb422ba4043a2cfb63210a461fdf5179
Maik Vinicius <maik_ga-eu@hotmail.com>
2020-06-02 17:51:21 -0300

    update site

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ questions.forEach(question => question.addEventListener('click', toggleQuestionF
 
 const courses = [
     {
-        title: 'Lógica de programação',
+        title: '1. Lógica de programação',
         description: 'É a forma como o desenvolvedor entende a comunicação a fim de programar uma função de um programa.',
         link: 'https://web.digitalinnovation.one/course/logica-de-programacao-essencial/learning/aea1ea26-fd56-417d-8272-6e15253f4405',
         color: 'green-color',
@@ -18,7 +18,7 @@ const courses = [
         modal: 'modal_example_1'
     },
     {
-        title: 'HTML5 e CSS3 básico',
+        title: '2. HTML5 e CSS3 básico',
         description: 'Aprenda a construir uma página WEB utilizando estrutura de HTML5 e defina estilos para elementos utilizando CSS3.',
         link: 'https://web.digitalinnovation.one/course/programando-interfaces-com-html5-e-css3/learning/f5066ef9-b542-43c0-a3c5-1519c61b84ee/',
         color: 'red-color',
@@ -26,7 +26,7 @@ const courses = [
         modal: 'modal_example_2'
     },
     {
-        title: 'Construindo páginas com Bootstrap',
+        title: '3. Construindo páginas com Bootstrap',
         description: 'Aprenda a utilizar o framework Bootstrap e aprofunde mais o seu conhecimento.',
         link: 'https://web.digitalinnovation.one/course/crie-paginas-responsivas-na-web-utilizando-um-poderoso-framework/learning/d5695916-44d4-4d47-9db4-0bc829264835/',
         color: 'red-color',
@@ -34,7 +34,7 @@ const courses = [
         modal: 'modal_example_3'
     },
     {
-        title: 'Iniciando com javascript',
+        title: '4. Iniciando com javascript',
         description: 'É uma das mais importantes linguagens front-end, e nesse curso você entenderá o porquê e como trabalhar com ela.',
         link: 'https://web.digitalinnovation.one/course/programacao-para-internet-com-javascript/learning/33e81edf-6f9b-4b97-b3c6-12d930cf0831/',
         color: 'blue-color',
@@ -85,7 +85,7 @@ function filterCourses(category) {
     
                         <a class="projeto" onclick="toggleModal('${course.modal}')">
                             <i class="far fa-file"></i>
-                            Com Projeto Proposto
+                            Após finalizar curso, clique aqui e faça o desafio!
                         </a>
                         <a href="${course.link}" target="_blank">
                         <button class="${course.color}">Iniciar</button>
@@ -111,7 +111,7 @@ function filterCourses(category) {
     
                         <a class="projeto" onclick="toggleModal('${course.modal}')">
                             <i class="far fa-file"></i>
-                            Com Projeto Proposto
+                            Após finalizar curso, clique aqui e faça o desafio!
                         </a>
                         <a href="${course.link}" target="_blank">
                         <button class="${course.color}">Iniciar</button>
